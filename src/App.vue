b4eacaa04a2125d699859c685b4361f319a33778
WILLWANG <wangziweichn@outlook.com>
2019-07-24 23:48:36 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <NavBar></NavBar>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar'
+export default {
+  components:{
+    NavBar : NavBar
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1">主页</el-menu-item>
+  <el-menu-item index="1">菜单</el-menu-item>
+  <el-menu-item index="1">管理</el-menu-item>
+  <el-menu-item index="1">关于我们</el-menu-item>
+</el-menu>
+</template>
+
+<script>
+   export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
